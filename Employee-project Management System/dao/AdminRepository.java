c3c130f7d2f7de2ec3637b1a71783a7505d0e422
Sanovar Fatema <111842669+Sanovar563@users.noreply.github.com>
2023-06-26 18:03:18 +0530

    Add files via upload

---
+++ Employee-project Management System/dao/AdminRepository.java
@@ -0,0 +1,9 @@
+//package com.example.ems.dao;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import com.example.ems.entities.Admin;
+//
+//public interface AdminRepository extends JpaRepository<Admin, Integer> {
+//    // Custom query methods, if any
+//}
---
+++ Employee-project Management System/dto/EmployeeProjectDTO.java
@@ -0,0 +1,10 @@
+package com.example.ems.dto;
+
+public class EmployeeProjectDTO {
+	 private int id;
+	    private EmployeeDTO employee;
+	    private ProjectDTO project;
+
+    // Constructors, getters, and setters
+}
+
