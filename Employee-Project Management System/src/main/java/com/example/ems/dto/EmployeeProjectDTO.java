095d1e00eff1691ff733be018a29dd72ef8e5f43
Sanovar Fatema <111842669+Sanovar563@users.noreply.github.com>
2023-06-29 23:57:15 +0530

    Update EmployeeProjectDTO.java

--- Employee-Project Management System/src/main/java/com/example/ems/dto/EmployeeProjectDTO.java
+++ Employee-Project Management System/src/main/java/com/example/ems/dto/EmployeeProjectDTO.java
@@ -1,10 +1,21 @@
 package com.example.ems.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class EmployeeProjectDTO {
 	 private int id;
 	    private EmployeeDTO employee;
 	    private ProjectDTO project;
 
-    // Constructors, getters, and setters
+   
 }
 
